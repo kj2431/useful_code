15d296a13d6b2693fc1a45c1acf48b421492f774
kj2431 <35583533+kj2431@users.noreply.github.com>
2021-04-20 15:03:27 +0800

    Update graphops.py

--- graphops.py
+++ graphops.py
@@ -2,3 +2,16 @@ import networkx as nx
 
 import numpy as np
 import pandas as pd
+
+def nx_connected_components(nodelist):
+    """
+    """
+    G = nx.Graph()
+    G.add_nodes_from(sum(nodelist, []))
+    e = [[(s[i],s[i+1]) for i in range(len(s)-1)] for s in nodelist]
+    
+    for i in e:
+        G.add_edges_from(i)
+    
+    return [list(i) for i in nx.connected_components(G)]
+
