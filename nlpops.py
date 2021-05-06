83471d100cecfab316dcb8ce70f58249ea37a2b2
kj2431 <35583533+kj2431@users.noreply.github.com>
2021-05-06 14:01:52 +0800

    Update nlpops.py

--- nlpops.py
+++ nlpops.py
@@ -12,6 +12,14 @@ import os
 import nltk
 
 def extract_number(string1):
-  """
-  """
-  return re.findall('\d*\.?\d+',string1)
+    """
+    """
+    return re.findall('\d*\.?\d+',string1)
+
+def ngram_to_nlist(string1, n=2):
+    """
+    """
+    ngram = nltk.ngrams(str(string1).split(), n)
+    nlist = [" ".join(list(grams)) for grams in ngram]
+    
+    return nlist
