f81d54764f69599d5b966e0e6e5518357ac8d552
kj2431 <35583533+kj2431@users.noreply.github.com>
2021-05-11 11:01:54 +0800

    Update nlpops.py

--- nlpops.py
+++ nlpops.py
@@ -16,6 +16,11 @@ def extract_number(string1):
     """
     return re.findall('\d*\.?\d+',string1)
 
+def extract_sentence(string1, keyword):
+    """
+    """
+    return re.findall(r"([^.]*?" + keyword + "[^.]*\.)", string1)
+
 def ngram_to_nlist(string1, n=2):
     """
     """
@@ -23,3 +28,18 @@ def ngram_to_nlist(string1, n=2):
     nlist = [" ".join(list(g)) for g in ngram]
     
     return nlist
+
+def ngram_search_engine(search_string, query):
+    """
+    """
+    ng = len(query.split(' '))
+    query = [query]
+    nlist = ngram_to_nlist(search_string, n=ng)
+    s = list(set(nlist) & set(query))
+    if len(s) > 0:
+        e1 = []
+        for j in range(0, len(s2)):
+            e1.append(extract_sentence(search_string, query))
+        return True, keyword[0], " ; ".join(e1[0])
+    else:
+        return False, False, False
